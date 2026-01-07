785348957903a2454b98a3f529e76273fa07a522
Predrag Mirkovic <pmirkovic@yahoo.com>
2026-01-08 02:14:51 +0800

    Auto commit: 2026-01-08 02:14:51

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -54,7 +54,7 @@ const About: React.FC<AboutProps> = () => {
                 <img
                   src="/images/nenad-photo.jpg"
                   alt="Nenad Mirkovic - Founder of VisionSF"
-                  className="w-full h-full object-cover object-bottom"
+                  className="w-full h-full object-cover object-top"
                 />
               </div>
             </div>
