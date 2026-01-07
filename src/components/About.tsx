5dd029762ecf5527ff75ae221adefad2afa27d01
Predrag Mirkovic <pmirkovic@yahoo.com>
2026-01-08 02:03:49 +0800

    fix: Replace with properly cropped founder photo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -54,7 +54,7 @@ const About: React.FC<AboutProps> = () => {
                 <img
                   src="/images/nenad-photo.jpg"
                   alt="Nenad Mirkovic - Founder of VisionSF"
-                  className="w-full h-full object-cover object-[center_25%]"
+                  className="w-full h-full object-cover"
                 />
               </div>
             </div>
